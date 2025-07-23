e5873f8a0d8809d79873a32143da4e4b1ab4e781
studentsusername <studentsemail>
2025-07-23 17:30:58 +0100

    part 3 session 11 hw #76

---
+++ CatchTheSpaceApp/CatchTheSpaceWeb/Pages/gameJquery.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatchTheSpaceWeb.Pages
+{
+    public class gameJqueryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
